73e314332c5ac3b8492031d6c2652b1f439a0fa0
Monty <dhanani.monty@gmail.com>
2016-04-02 22:55:17 -0400

    added time loop

--- client/cutranspo.py
+++ client/cutranspo.py
@@ -2,9 +2,12 @@ import urllib
 import urllib2
 import json
 from pprint import pprint
+import sched, time
 
 deviceName = 'rpi'
 password = '1234'
+
+s = sched.scheduler(time.time, time.sleep)
 
 try:
 	url = 'http://172.17.98.162:3000/api/Devices'
@@ -17,31 +20,35 @@ try:
 except urllib2.HTTPError:
 	print "Already Registered"
 
-try:
-	url = 'http://172.17.98.162:3000/api/Devices/login'
-	values = {'deviceName' : deviceName,'password' : password}
-	data = urllib.urlencode(values)
-	req = urllib2.Request(url, data)
-	print req
-	response = urllib2.urlopen(req)
-	access_token = response.read()
-except urllib2.HTTPError:
-	print "Login failed"
+def getNextTimes(sc):
+	try:
+		url = 'http://172.17.98.162:3000/api/Devices/login'
+		values = {'deviceName' : deviceName,'password' : password}
+		data = urllib.urlencode(values)
+		req = urllib2.Request(url, data)
+		print req
+		response = urllib2.urlopen(req)
+		access_token = response.read()
+	except urllib2.HTTPError:
+		print "Login failed"
 
-query = urllib2.urlencode({"busNo" : "104,7,4,111"}, {"access_token" : access_token})
+	query = urllib.urlencode({"busNo" : "104,7,4,111"}, {"access_token" : access_token})
 
-req = urllib2.Request('http://172.17.98.162:3000/api/Devices/getTimes?' + query)
+	req = urllib2.Request('http://172.17.98.162:3000/api/Devices/getTimes?' + query)
 
 
-# json_input = '{ "one": { "list": [ {"item":"A"},{"item":"B"} ] }, "two": { "list": [ {"item":"A"},{"item":"B"} ] } }'
+	# json_input = '{ "one": { "list": [ {"item":"A"},{"item":"B"} ] }, "two": { "list": [ {"item":"A"},{"item":"B"} ] } }'
 
-try:
-    decoded = json.loads(login_json)
- 
-    # pretty printing of json-formatted string
-    print json.dumps(decoded, sort_keys=True, indent=4)
- 
-    print "Device Name", decoded['deviceName']
-    print "Email", decoded['email']
-except (ValueError, KeyError, TypeError):
-    print "JSON format error"
+	try:
+	    decoded = json.loads(login_json)
+	 
+	    # pretty printing of json-formatted string
+	    print json.dumps(decoded, sort_keys=True, indent=4)
+	 
+	    print "Device Name", decoded['deviceName']
+	    print "Email", decoded['email']
+	except (ValueError, KeyError, TypeError):
+	    print "JSON format error"
+
+s.enter(10, 1, getNextTimes, (s,))
+s.run()
