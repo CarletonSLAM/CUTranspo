345ca7f162694d947191dbfb7dc5c32e2928d5de
Monty <dhanani.monty@gmail.com>
2016-04-02 22:07:25 -0400

    put script in client folder

---
+++ client/cutranspo.py
@@ -0,0 +1,47 @@
+import urllib
+import urllib2
+import json
+from pprint import pprint
+
+deviceName = 'rpi'
+password = '1234'
+
+try:
+	url = 'http://172.17.98.162:3000/api/Devices'
+	values = {'deviceName' : deviceName,'password' : password, 'stopNo' : '5813'}
+	data = urllib.urlencode(values)
+	req = urllib2.Request(url, data)
+	print req
+	response = urllib2.urlopen(req)
+	login_json = response.read()
+except urllib2.HTTPError:
+	print "Already Registered"
+
+try:
+	url = 'http://172.17.98.162:3000/api/Devices/login'
+	values = {'deviceName' : deviceName,'password' : password}
+	data = urllib.urlencode(values)
+	req = urllib2.Request(url, data)
+	print req
+	response = urllib2.urlopen(req)
+	access_token = response.read()
+except urllib2.HTTPError:
+	print "Login failed"
+
+query = urllib2.urlencode({"busNo" : "104,7,4,111"}, {"access_token" : access_token})
+
+req = urllib2.Request('http://172.17.98.162:3000/api/Devices/getTimes?' + query)
+
+
+# json_input = '{ "one": { "list": [ {"item":"A"},{"item":"B"} ] }, "two": { "list": [ {"item":"A"},{"item":"B"} ] } }'
+
+try:
+    decoded = json.loads(login_json)
+ 
+    # pretty printing of json-formatted string
+    print json.dumps(decoded, sort_keys=True, indent=4)
+ 
+    print "Device Name", decoded['deviceName']
+    print "Email", decoded['email']
+except (ValueError, KeyError, TypeError):
+    print "JSON format error"
