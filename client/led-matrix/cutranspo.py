c0436b593dbe2dea5765c61249f20d1021a625b7
Monty <dhanani.monty@gmail.com>
2016-04-08 11:53:51 -0400

    Refactored

--- client/led-matrix/cutranspo.py
+++ client/led-matrix/cutranspo.py
@@ -7,7 +7,8 @@ from subprocess import Popen, PIPE
 import os
 import time
 
-deviceName = 'rpi'
+stopNo = '5813'
+deviceName = 'rpi' + stopNo
 password = '1234'
 
 s = sched.scheduler(time.time, time.sleep)
@@ -15,7 +16,7 @@ PROGRAM_PATH = os.getcwd() + '/display-text'
 
 try:
 	url = 'https://cu-transpo.herokuapp.com/api/Devices'
-	values = {'deviceName' : deviceName,'password' : password, 'stopNo' : '5813'}
+	values = {'deviceName' : deviceName,'password' : password, 'stopNo' : stopNo}
 	data = urllib.urlencode(values)
 	req = urllib2.Request(url, data)
 	print req
@@ -53,24 +54,29 @@ def getNextTimes(sc):
 
 		for i in li:
 			if len(decoded['response']['data'][i][0]['times']) > 0:
+				stream = Popen(["sudo", PROGRAM_PATH, "200,10,0", "80", i + decoded['response']['data'][i][0]['dest'], decoded['response']['data'][i][0]['times'][0] + "  " + decoded['response']['data'][i][0]['times'][1]], stdin=PIPE, stderr=PIPE, universal_newlines=True)
+				time.sleep(5)
+				print stream.terminate()
 				print i + "   " + decoded['response']['data'][i][0]['dest']
 				print decoded['response']['data'][i][0]['times']
 			else:
+				stream = Popen(["sudo", PROGRAM_PATH, "200,10,0", "80", i + decoded['response']['data'][i][1]['dest'], decoded['response']['data'][i][1]['times'][0] + "  " + decoded['response']['data'][i][1]['times'][1]], stdin=PIPE, stderr=PIPE, universal_newlines=True)
+				time.sleep(5)
+				print stream.terminate()
 				print i + "   " + decoded['response']['data'][i][1]['dest']
 				print decoded['response']['data'][i][1]['times']
 		
 	except (ValueError, KeyError, TypeError):
 		print "JSON format error"
 
-	stream = Popen(["sudo", PROGRAM_PATH, "200,10,0", "80", "4 " + destination_4, times_4[0] + "  " + times_4[1]], stdin=PIPE, stderr=PIPE, universal_newlines=True)
-	print stream
-	
-	time.sleep(5);
-	print stream.terminate()
+	# stream = Popen(["sudo", PROGRAM_PATH, "200,10,0", "80", "4 " + destination_4, times_4[0] + "  " + times_4[1]], stdin=PIPE, stderr=PIPE, universal_newlines=True)
+	# print stream	
+	# time.sleep(5);
+	# print stream.terminate()
 		
-	stream = Popen(["sudo", PROGRAM_PATH, "200,10,0", "80", "104 " + destination_104, times_104[0] + "  " + times_104[1]], stdin=PIPE, stderr=PIPE, universal_newlines=True)
-	time.sleep(5)
-	print stream.terminate()
+	# stream = Popen(["sudo", PROGRAM_PATH, "200,10,0", "80", "104 " + destination_104, times_104[0] + "  " + times_104[1]], stdin=PIPE, stderr=PIPE, universal_newlines=True)
+	# time.sleep(5)
+	# print stream.terminate()
 	s.enter(0.05, 1,getNextTimes, (s,))
-	s.run()	
+	s.run()
 getNextTimes(s)
